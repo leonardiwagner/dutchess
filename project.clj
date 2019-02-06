2d75add44b81b7e7ee04cda5aab04cf004d37a24
Wagner Leonardi <leonardiwagner@gmail.com>
2019-02-06 17:22:47 -0200

    sentences reader improvements

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject dutchess "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"] [enlive "1.1.5"]]
   :main ^:skip-aot dutchess.main
+  :jvm-opts ["-Xmx6g"]
   :target-path "target/%s")
