a6400234aefa0407b20fad118f7b009e60105c54
Wagner Leonardi <leonardiwagner@gmail.com>
2021-06-22 16:02:07 +0200

    update

--- js/run.sh
+++ js/run.sh
@@ -3,9 +3,9 @@ sudo apt update
 sudo apt install -y unzip
 sudo apt install -y build-essential
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-source ~/.bashrc
+source /home/ubuntu/.bashrc
 nvm install 12
-wget  https://dutchess-movies-english.s3.eu-north-1.amazonaws.com/english/movies.zip
+wget  https://dutchess-movies-english.s3.eu-north-1.amazonaws.com/dutch/dutch.zip
 mkdir movies1 movies2 movies3 movies4
 unzip movies.zip
 cd movies
