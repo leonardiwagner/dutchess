ea79af3d795beaf059d2a0c5739a7433661a51d5
Wagner Leonardi <leonardiwagner@gmail.com>
2021-06-22 09:35:26 +0200

    update script

--- js/run.sh
+++ js/run.sh
@@ -4,7 +4,6 @@ sudo apt install -y build-essential
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 nvm install 12
-git clone https://github.com/leonardiwagner/dutchess.git
 wget  https://dutchess-movies-english.s3.eu-north-1.amazonaws.com/english/movies.zip
 mkdir movies1 movies2 movies3 movies4
 unzip movies.zip
